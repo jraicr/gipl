b18184d0456a8b675cb0daaee098715b102fbe08
Rai <jraicr@gmail.com>
2023-02-10 19:33:59 +0000

    Añade formulario de creación de incidencias (Incompleto)

--- gipl_webapp/resources/views/app/incidences/create.blade.php
+++ gipl_webapp/resources/views/app/incidences/create.blade.php
@@ -10,7 +10,7 @@
 
     <div class="card">
         <div class="card-header">
-            <a class="btn btn-primary" href="{{route('app.incidences.create')}}">Crear incidencia</a>
+            <a class="btn btn-primary" href="{{ route('app.incidences.create') }}">Crear incidencia</a>
         </div>
         <div class="card-body">
             {!! Form::open(['route' => 'app.incidences.store']) !!}
@@ -19,9 +19,44 @@
                 {!! Form::text('description', null, ['class' => 'form-control', 'placeholder' => 'Describe la incidencia']) !!}
             </div>
             <div class="form-group">
+                <p class="font-weight-bold">Estado</p>
+                <div class="form-check form-check-inline">
 
-                {!! Form::label('description', 'Descripción') !!}
+
+                    @foreach ($states as $state)
+                        {!! Form::radio('state', $state->name, null, ['class' => 'form-check-input']) !!}
+                        {!! Form::label('state', $state->name, ['class' => 'form-check-label mr-2']) !!}
+                    @endforeach
+
+
+                </div>
             </div>
+            <div class="form-group">
+                {!! Form::label('classroom', 'Aula:') !!}
+                <input list="classrooms" id="classroom" name="classroom" />
+                <datalist id="classrooms">
+
+                    @foreach ($classrooms as $classroom)
+                        <option value="{{ $classroom->num }}">
+                    @endforeach
+
+                </datalist>
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('computer', 'Equipo:') !!}
+                <input list="computers" id="computer" name="computer" />
+
+
+                {{-- @foreach ($computers->where('classroom_id', $selectedClassroom) as $computersAux)
+                    @foreach ($computersAux as $computer)
+                        <p>{{$computer}}</p>
+                    @endforeach
+                @endforeach --}}
+
+
+            </div>
+
             {!! Form::close() !!}
         </div>
     </div>
