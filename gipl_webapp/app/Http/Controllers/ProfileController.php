f14dce80230dba62147713d4ba3a74ea0af41da9
Rai <jraicr@gmail.com>
2023-02-12 01:58:59 +0000

    Arregla problema con Livewire en la vista show del perfil de usuario.
    - Refactoriza ubicación de vista a resources/views/app/profile/show.blade.php
    - Modifica la ruta de la vista show profile de "app.profile" a  "app.profile.show"

--- gipl_webapp/app/Http/Controllers/ProfileController.php
+++ gipl_webapp/app/Http/Controllers/ProfileController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     public function index() {
-        return view('app.profile');
+        return view('app.profile.show');
     }
 }
---
+++ gipl_webapp/resources/views/app/profile/show.blade.php
@@ -0,0 +1,51 @@
+@extends('adminlte::page')
+
+@section('title', 'GIPL - Perfil')
+
+@section('content_header')
+    <h1>Perfil del usuario</h1>
+@stop
+
+@section('js')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@stop
+
+@section('content')
+    <div>
+        <div class="py-10 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+                @livewire('profile.update-profile-information-form')
+
+                <x-jet-section-border />
+            @endif
+
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.update-password-form')
+                </div>
+
+                <x-jet-section-border />
+            @endif
+
+            @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.two-factor-authentication-form')
+                </div>
+
+                <x-jet-section-border />
+            @endif
+
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.logout-other-browser-sessions-form')
+            </div>
+
+            @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
+                <x-jet-section-border />
+
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.delete-user-form')
+                </div>
+            @endif
+        </div>
+    </div>
+@endsection
