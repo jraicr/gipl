1694a5092d7a74e4a96e837c3751cf38da9e6e5c
Rai <jraicr@gmail.com>
2023-02-18 14:01:41 +0000

    Añade modal de confirmación cuando se trata de eliminar una incidencia

--- gipl_webapp/resources/views/app/incidences/index.blade.php
+++ gipl_webapp/resources/views/app/incidences/index.blade.php
@@ -64,10 +64,11 @@
                             @endif
                             <td>{{ $incidence->created_at }}</td>
 
-                            <td><a class="btn btn-primary" href="{{ route('app.incidences.edit', $incidence) }}">Editar</td>
+                            <td><a class="btn btn-primary" href="{{ route('app.incidences.edit', $incidence) }}">Editar
+                            </td>
                             <td>
                                 {!! Form::model('incidence', ['route' => ['app.incidences.destroy', $incidence], 'method' => 'DELETE']) !!}
-                                {!! Form::submit('Eliminar', ['class' => 'btn btn-danger']) !!}
+                                {!! Form::submit('Eliminar', ['class' => 'btn btn-danger remove-incidence']) !!}
                                 {!! Form::close() !!}
                             </td>
                         </tr>
@@ -84,8 +85,112 @@
 
 {{-- @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
+@stop --}}
 
 @section('js')
-    <script> console.log('Hola!'); </script>
-@stop --}}
+    <script>
+        const listenForRemove = () => {
+            let removeButtons;
+            let removeConfirmationDialog;
+            let currentIncidenceToRemove;
+
+            let contentContainer;
+
+            init();
+
+
+            function init() {
+                cacheElements();
+                setupEventHandlers();
+            }
+
+            function cacheElements() {
+                removeButtons = document.querySelectorAll('.remove-incidence');
+                contentContainer = document.querySelector(".container-fluid");
+            }
+
+            function setupEventHandlers() {
+
+                // Evento al hacer click en el botón eliminar incidencia
+                removeButtons.forEach(btn => {
+                    btn.addEventListener('click', (e) => {
+                        showConfirmationModal(e);
+                        e.target.parentNode.parentNode.parentNode.style.backgroundColor = "#ff7b3473";
+                    });
+                });
+            }
+
+            function showConfirmationModal(e) {
+
+                e.preventDefault();  // Evitamos que el botón de eliminar ejecute el submit.
+
+                // Cacheamos el elemento del botón que se ha pulsado originalmente,
+                //  para rescatar la fila de la incidencia en la tabla posteriormente.
+                currentIncidenceToRemove = e.target;
+
+                removeConfirmationDialog = createDialog('¿Desea borrar esta incidencia?', contentContainer);
+                removeConfirmationDialog.showModal();
+            }
+
+            // Gestiona la decisión del usuario en el dialogo de confirmación de eliminación de incidencia
+            function handlerUserInput(e, returnValue) {
+                if (returnValue == 'remove') {
+                    console.log(e.target);
+                    currentIncidenceToRemove.form.submit();
+                    currentIncidenceToRemove.parentNode.parentNode.parentNode.style.backgroundColor = "#ff7b7b";
+
+                } else {
+                    currentIncidenceToRemove.parentNode.parentNode.parentNode.style.backgroundColor = null;
+                }
+
+                currentIncidenceToRemove.parentNode.parentNode.parentNode.style.transition = "all 300ms linear";
+                currentIncidenceToRemove = undefined;
+            }
+
+            // Crea un modal con el elemento html Dialog.
+            // Más info sobre <dialog>: https://developer.mozilla.org/es/docs/Web/HTML/Element/dialog
+            function createDialog(text, container) {
+                let dialogElement = document.createElement('dialog');
+                let sectionElement = document.createElement('section');
+                let textParagraphElement = document.createElement('p');
+                let dialogForm = document.createElement('form');
+                let buttonConfirm = document.createElement('button');
+                let buttonCancel = document.createElement('button');
+
+                dialogElement.id = 'remove-confirmation';
+                textParagraphElement.innerText = text;
+                dialogForm.method = "dialog";
+
+                buttonConfirm.type = "submit";
+                buttonConfirm.classList.add("btn", "btn-danger");
+                buttonConfirm.value = "remove";
+                buttonConfirm.id = 'remove-confirm-btn';
+                buttonConfirm.innerText = 'Confirmar';
+
+                buttonCancel.type = "submit";
+                buttonCancel.classList.add("btn", "btn-secondary", "float-right");
+                buttonCancel.value = "cancel";
+                buttonCancel.id = 'remove-cancel-btn';
+                buttonCancel.innerText = 'Cancelar';
+
+
+                dialogForm.appendChild(buttonConfirm);
+                dialogForm.appendChild(buttonCancel);
+                sectionElement.appendChild(textParagraphElement);
+                dialogElement.appendChild(sectionElement);
+                dialogElement.appendChild(dialogForm);
+
+                // Evento al hacer click en cualquier botón del elemento html Dialog
+                dialogElement.addEventListener('close', (e) => {
+                    handlerUserInput(e, dialogElement.returnValue);
+                });
+
+                container.appendChild(dialogElement);
+
+                return dialogElement;
+            }
+        }
+
+        listenForRemove();
+    </script>
+@stop
