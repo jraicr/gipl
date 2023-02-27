987023ea4547fa20e32c96f54409ad3e7345f3a1
Donovanmar <donovanmar30@gmail.com>
2023-02-27 12:41:15 +0000

    Define la asignación masiva y control de los campos en vista ordenadores

---
+++ gipl_webapp/app/Http/Requests/ComputerRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ComputerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        // $computer = $this->route()->parameter('computer');
+
+        $rules = [
+            'num' => [
+                        'required',
+                        'unique:App\Models\Computer',
+                        'regex:/^INFORMATICA\-[0-9]{3}$/',
+                     ]
+        ];
+
+        // if ($computer) {
+        //     $rules['num'] = 'required|unique:App\Models\Computer|regex:/^INFORMATICA\-[0-9]{3}$/'. $computer->id;
+        // }
+
+        return $rules;
+    }
+}
