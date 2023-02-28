0627344aa6b4958500349e37890f79f039a1ef3b
Rai <jraicr@gmail.com>
2023-02-28 10:16:52 +0000

    Arregla la dirección del enlace en el logo de la web en formularios de login y registro

--- gipl_webapp/resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ gipl_webapp/resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
-<a href="/" class="flex items-center flex-shrink-0">
+<a href="{{route('app.dashboard')}}" class="flex items-center flex-shrink-0">
     <img class="block w-35 lg:hidden" src="{{ Asset('assets/pics/logo-390.png') }}" alt="GIPL">
     <img class="hidden w-35 lg:block" src="{{ Asset('assets/pics/logo-390.png') }}" alt="GIPL">
 </a>
