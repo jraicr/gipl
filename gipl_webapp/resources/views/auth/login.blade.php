bf08bdb0094470544cfc8ccb12ffcf1763710232
Rai <jraicr@gmail.com>
2023-02-28 02:15:53 +0000

    Revierte cambios anteriores. Instala dependencia de Laravel Lang e introduce soporte para el castellano en Laravel y Jetstream

--- gipl_webapp/resources/views/auth/login.blade.php
+++ gipl_webapp/resources/views/auth/login.blade.php
@@ -21,26 +21,26 @@
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Contraseña') }}" />
+                <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Recuérdame') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('¿Olvidaste la contraseña?') }}
+                        {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 ml-4" href="{{ route('register') }}">
-                    {{ __('Registrarse') }}
+                    {{ __('Register') }}
                 </a>
 
                 <x-jet-button class="ml-4">
