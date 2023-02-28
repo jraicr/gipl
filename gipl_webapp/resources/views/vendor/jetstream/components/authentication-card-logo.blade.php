e11d23d90350dc1dee61fd18bd461a5d4fa4241e
Rai <jraicr@gmail.com>
2023-02-28 01:35:21 +0000

    Corrige ruta de logo en la vista del login

--- gipl_webapp/resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ gipl_webapp/resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
 <a href="/" class="flex items-center flex-shrink-0">
-    <img class="block lg:hidden" src="{{ Storage::url('pics/logo-390.png') }}" alt="GIPL">
-    <img class="hidden w-36 lg:block" src="{{ Storage::url('pics/logo-390.png') }}" alt="GIPL">
+    <img class="block w-35 lg:hidden" src="{{ Asset('assets/pics/logo-390.png') }}" alt="GIPL">
+    <img class="hidden w-35 lg:block" src="{{ Asset('assets/pics/logo-390.png') }}" alt="GIPL">
 </a>
